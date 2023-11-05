7681a55dcb7b53fc7e092f43f3e6fc1fa6f93e5d
Junior Costa <120533852+Junincss@users.noreply.github.com>
2023-11-05 15:32:08 -0300

    Atualização de classes
    Terminar de desenvolver a classe moto em casa.

--- Jr-Aula11/classes-Objetos.py
+++ Jr-Aula11/classes-Objetos.py
@@ -35,8 +35,79 @@ gol.acelerar()
 
 gol.mostrarVelocidade()
 
+"""
+
+Criando uma nova classe para motos:
+
+"""
 
 
 
 
+class Moto:
+    cor = ""
+    modelo = ""
+    fabricante = ""
+    ano = 0
+    buzina = True
+    freio = True
+    velocimetro = 0
+
+    def __init__(self, cor, modelo, fabricante,ano, buzina, freio,velocimetro):
+        self.cor = cor
+        self.modelo = modelo
+        self.fabricante = fabricante
+        self.ano = ano
+        self.buzina = buzina
+        self.freio = freio
+        self.velocimetro = velocimetro
+
+
+    def ligar(self):
+        print(f"Sua {self.modelo} está ligada!")
+
+    def acelerar(self):
+        print("acelerando")
+        self.velocimetro += 10
+    
+    def reduzir(self):
+        self.velocimetro -= 10
+        if(self.velocimetro >=0):
+            print(f"Reduzindo velocidade\n Sua velocidade atual é : {self.velocimetro}Km/h")
+    
+    def velocidadeAtual(self):
+        print(f"Sua velocidade atual é {self.velocimetro} km/h")
+        while(True):
+            question = input("Continuar acelerando ? (S/N)\n>  ").upper()
+            match(question):
+                case "S":
+                    self.velocimetro +=10
+                    print(f"Você está a {self.velocimetro} km/h")
+                    continue
+                case "N":
+                    print(f"Velocidade atual mantida em {self.velocimetro} km/h")
+                    choice = input("Deseja reduzir a velocidade atual ? (S/N)").upper()
+                    if(choice =="S"):
+                        self.reduzir()
+                    break
+                case _:
+                    print("Opção Inválida - tente novamente!")
+                    continue
+                
+
+
+
+
+
+
+
+mt03 = Moto("Preta", "Master Of Toque 03", "Yamaha",2018,True,True,0)
+
+
+mt03.ligar()
+
+
+mt03.acelerar()
+
+mt03.velocidadeAtual()
 
